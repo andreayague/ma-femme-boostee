f04a9b77eb90b4e7ebdbcd4340ad5aebe1121aad
andreayague <andrea.yagueg06@gmail.com>
2026-02-03 21:28:55 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 # Configuración de la página
-st.set_page_config(page_title="Para mi niña ❤️", page_icon="🌹")
+st.set_page_config(page_title="Pour ma femme boostee, l'amour de ma vie ❤️", page_icon="🌹")
 
 # Estilo personalizado con CSS
 st.markdown("""
@@ -30,23 +30,23 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("Tengo una pregunta muy importante... 🌹")
+st.title("J'ai une question tres importante pour toi... 🌹")
 
 # Imagen o GIF romántico
-st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJueGZ3bmZqZzRyeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4JnB0PWEmZXA9djFfaW50ZXJuYWxfZ2lmX2J5X2lkJmN0PWc/c76IJLufpNwSULPk77/giphy.gif")
+st.image("https://i.pinimg.com/originals/f1/09/3d/f1093dd6dce5892d2b74c6e8d8c1c909.gif")
 
-st.write("### Llevo tiempo pensando en esto y quería decírtelo de una forma especial:")
+st.write("### Voudrais-tu être mon Saint Valentin?")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("¡SÍ! ❤️"):
+    if st.button("OUI❤️"):
         st.balloons()
         st.success("¡ME HACES EL MÁS FELIZ DEL MUNDO! 💍✨")
         st.write("Prometo que será un día increíble. ¡Te amo!")
 
 with col2:
     # Un poco de humor: el botón de "No" que es difícil de clickear (opcional)
-    no_button = st.button("No 😢")
+    no_button = st.button("Non 😢")
     if no_button:
-        st.warning("Esa opción no está disponible en este momento. Intenta de nuevo. 😉")
+        st.warning("Option pas disponible. Essaye encore une fois 😉")
